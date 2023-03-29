8deba25a6310f8b67e51dbd15f27d392f305892e
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-03-29 15:35:42 +0200

    inserimento stor

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    name: 'AppHeader',
+    props: {
+        titolo: String
+    }
+}
+</script>
+
+<template>
+    <div class="headerContainer">
+        <h1>{{ titolo }}</h1>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.headerContainer {
+    width: 100%;
+    background-color: white;
+    height: 80px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
